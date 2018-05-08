2c67a4f4ba86229408006cc0337f4e687bd0fe47
Mike van Emmerik <m.van.emmerik@everest.nl>
2018-05-08 11:21:54 +0200

    BQ-4286 & BQ-4385 Projectstructure setup ts lint + fixed them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
 import {BlueriqModule} from '@blueriq/angular';
 import {V1BackendModule} from '@blueriq/angular/backend/v1';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
+import {AppComponent} from './app.component';
 import {MaterialModule} from './material/material.module';
 
-const COMPONENTS = [
-
-]
+// const COMPONENTS = [
+//   PageComponent
+// ]
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
     // COMPONENTS
   ],
   imports: [
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,3 +1,11 @@
+import {A11yModule} from '@angular/cdk/a11y';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import {BidiModule} from '@angular/cdk/bidi';
+import {ObserversModule} from '@angular/cdk/observers';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {PlatformModule} from '@angular/cdk/platform';
+import {PortalModule} from '@angular/cdk/portal';
+import {CdkTableModule} from '@angular/cdk/table';
 import {NgModule} from '@angular/core';
 import {
   MatAutocompleteModule,
@@ -33,14 +41,6 @@ import {
   MatToolbarModule,
   MatTooltipModule
 } from '@angular/material';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
-import {A11yModule} from '@angular/cdk/a11y';
-import {BidiModule} from '@angular/cdk/bidi';
-import {OverlayModule} from '@angular/cdk/overlay';
-import {PlatformModule} from '@angular/cdk/platform';
-import {ObserversModule} from '@angular/cdk/observers';
-import {PortalModule} from '@angular/cdk/portal';
 
 /**
  * NgModule that includes all Material modules that are required to serve the demo-app.
