8417a5d40f2e6965b2a17ebb993c5aa49da45d06
Geert Graat <g.graat@everest.nl>
2018-05-17 14:19:17 +0200

    Added test to fix build

---
+++ frontend/src/app/blueriq/material/app.component.spec.ts
@@ -0,0 +1,22 @@
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {BlueriqTestingModule} from "@blueriq/angular/testing";
+
+describe('AppComponent', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      imports: [
+        BlueriqTestingModule
+      ]
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+});
