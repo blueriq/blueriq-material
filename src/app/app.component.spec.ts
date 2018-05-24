6d0ec636741cad952d4a152fc763c93b3bfb8c80
Geert Graat <g.graat@everest.nl>
2018-05-24 16:18:48 +0200

    BQ-4490: fix failing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,25 @@
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {BlueriqTestingModule} from '@blueriq/angular/testing';
+import {RouterModule} from "@angular/router";
+import {APP_BASE_HREF} from "@angular/common";
+import {ProjectComponent} from "./blueriq/project/project.component";
 
 describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ProjectComponent
       ],
       imports: [
-        BlueriqTestingModule
-      ]
+        BlueriqTestingModule,
+        RouterModule.forRoot([]),
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
