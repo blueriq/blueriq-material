4acb73bb65e9e9b1b607427fafa7eeff0ae9c9a2
Felix Janssen <f.janssen@everest.nl>
2018-05-29 16:13:48 +0200

    feat(elements): button #BQ-4491 #BQ-4604
    Make button color configurable with presentation styles

--- src/app/blueriq/material/button/button.component.ts
+++ src/app/blueriq/material/button/button.component.ts
@@ -1,7 +1,6 @@
-import {Component, Host} from '@angular/core';
-import {BlueriqComponent, BlueriqSession} from '@blueriq/angular';
-import {Button} from '@blueriq/core';
-
+import { Component, Host } from '@angular/core';
+import { BlueriqComponent, BlueriqSession } from '@blueriq/angular';
+import { Button } from '@blueriq/core';
 
 @Component({
   templateUrl: './button.component.html',
@@ -14,8 +13,21 @@ import {Button} from '@blueriq/core';
 
 export class ButtonComponent {
 
-
   constructor(@Host() public button: Button, private session: BlueriqSession) {
+  }
+
+  getColor(): string {
+    if (this.button.styles.has('Primary') || this.button.styles.has('button_primary')) {
+      return 'primary';
+    } else if (this.button.styles.has('Accent')) {
+      return 'accent';
+    } else {
+      return null;
+    }
+  }
+
+  isDisabled(): boolean {
+    return this.button.disabled || this.button.styles.has('Disabled');
   }
 
   onClick(): void {
