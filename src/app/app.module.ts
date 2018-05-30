1f9e56ebbe84a5332c0033c6f7460a383645d3e8
Geert Graat <g.graat@everest.nl>
2018-05-30 10:29:06 +0200

    build(router): make the router suitable for local development #BQ-4491

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { BlueriqComponents, BlueriqModule } from '@blueriq/angular';
-import { V1BackendModule } from '@blueriq/angular/backend/v1';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { AppComponent } from './app.component';
-import { ButtonComponent } from './blueriq/material/button/button.component';
-import { ContainerComponent } from './blueriq/material/container/container.component';
-import { FieldComponent } from './blueriq/material/field/field.component';
-import { MaterialModule } from './blueriq/material/material/material.module';
-import { PageComponent } from './blueriq/material/page/page.component';
-import { TextItemComponent } from './blueriq/material/textitem/textitem.component';
-import { ProjectComponent } from './blueriq/project/project.component';
-import { CheckboxComponent } from './blueriq/material/form-controls/checkbox/checkbox.component';
+import {isDevMode, NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {BlueriqComponents, BlueriqModule} from '@blueriq/angular';
+import {V1BackendModule} from '@blueriq/angular/backend/v1';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {AppComponent} from './app.component';
+import {ButtonComponent} from './blueriq/material/button/button.component';
+import {ContainerComponent} from './blueriq/material/container/container.component';
+import {FieldComponent} from './blueriq/material/field/field.component';
+import {MaterialModule} from './blueriq/material/material/material.module';
+import {PageComponent} from './blueriq/material/page/page.component';
+import {TextItemComponent} from './blueriq/material/textitem/textitem.component';
+import {ProjectComponent} from './blueriq/project/project.component';
+import {CheckboxComponent} from './blueriq/material/form-controls/checkbox/checkbox.component';
 
-const routes: Routes = [
-  { path: 'session/:sessionId', component: ProjectComponent },
-];
+const routes: Routes = isDevMode() ?
+  [{path: '**', component: ProjectComponent}] :
+  [{path: 'session/:sessionId', component: ProjectComponent}];
 
 const COMPONENTS = [
   PageComponent,
@@ -38,12 +38,12 @@ const COMPONENTS = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, ) ,
+    RouterModule.forRoot(routes,),
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     BlueriqModule.forRoot(),
     V1BackendModule.forRoot({
-      baseUrl: '../server',
+      baseUrl: isDevMode ? '/Runtime' : '../server',
     }),
     BrowserAnimationsModule,
     MaterialModule,
