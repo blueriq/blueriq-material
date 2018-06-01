26d4b4d986fb8511145ac3cd2e7059c17966a50c
Mike van Emmerik <m.van.emmerik@everest.nl>
2018-06-01 15:48:30 +0200

    merge conflicsz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule, Routes} from '@angular/router';
-import {BlueriqComponents, BlueriqModule} from '@blueriq/angular';
-import {V1BackendModule} from '@blueriq/angular/backend/v1';
-import {EffectsModule} from '@ngrx/effects';
-import {StoreModule} from '@ngrx/store';
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { BlueriqComponents, BlueriqModule } from '@blueriq/angular';
+import { V1BackendModule } from '@blueriq/angular/backend/v1';
+import { BlueriqFormsModule } from '@blueriq/angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { ElementComponent } from './blueriq/generic/element/element.component';
+import { ButtonComponent } from './blueriq/material/button/button.component';
+import { ContainerComponent } from './blueriq/material/container/container.component';
+import { FieldComponent } from './blueriq/material/field/field.component';
+import { MaterialModule } from './blueriq/material/material/material.module';
+import { PageComponent } from './blueriq/material/page/page.component';
+import { TextItemComponent } from './blueriq/material/textitem/textitem.component';
 import { ProjectComponent } from './blueriq/project/project.component';
-import {ButtonComponent} from './blueriq/material/button/button.component';
-import {ContainerComponent} from './blueriq/material/container/container.component';
-import {FieldComponent} from './blueriq/material/field/field.component';
-import {MaterialModule} from './blueriq/material/material/material.module';
-import {PageComponent} from './blueriq/material/page/page.component';
-import {TextItemComponent} from './blueriq/material/textitem/textitem.component';
 import {TableComponent} from './blueriq/material/table/table.component';
 
 const routes: Routes = [
@@ -22,24 +24,29 @@ const routes: Routes = [
   { path: '**', component: ProjectComponent }
 ];
 
-const COMPONENTS = [
+const BQ_COMPONENTS = [
   PageComponent,
   ContainerComponent,
   TextItemComponent,
   ButtonComponent,
   FieldComponent,
-  TableComponent,
+  TableComponent
+];
+
+const BQ_MAT_COMPONENTS = [
+  ElementComponent
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectComponent,
-    COMPONENTS
+    BQ_COMPONENTS,
+    BQ_MAT_COMPONENTS
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, ) ,
+    RouterModule.forRoot(routes),
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     BlueriqModule.forRoot(),
@@ -47,12 +54,13 @@ const COMPONENTS = [
       baseUrl: '../server',
     }),
     BrowserAnimationsModule,
+    BlueriqFormsModule.forRoot(),
     MaterialModule,
     FormsModule,
     ReactiveFormsModule
   ],
   providers: [
-    BlueriqComponents.register(COMPONENTS),
+    BlueriqComponents.register(BQ_COMPONENTS)
   ],
   bootstrap: [AppComponent]
 })
