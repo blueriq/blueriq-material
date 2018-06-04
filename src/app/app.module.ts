7b18dbe9b09c9728e4cb54cc0fbcabf2d90b7773
r.de.haard <r.de.haard@everest.nl>
2018-06-04 09:32:14 +0200

    build(core): removed unused import #BQ-4289

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { isDevMode, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
