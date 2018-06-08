53781bce5939394e4a4c4f0fa7a37bb5a31350d2
Mike van Emmerik <m.van.emmerik@everest.nl>
2018-06-08 17:36:10 +0200

    test(table): WIP table test #BQ-4580 \n\n WIP tests for table

--- src/app/blueriq/material/table/table.component.spec.ts
+++ src/app/blueriq/material/table/table.component.spec.ts
@@ -1,49 +1,122 @@
 ///<reference path="../../../../../node_modules/@types/jasmine/index.d.ts"/>
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BlueriqComponents, BlueriqModule } from '@blueriq/angular';
+import { Table } from '@blueriq/angular/lists';
 import { BlueriqSessionTemplate, BlueriqTestingModule } from '@blueriq/angular/testing';
-import { ContainerTemplate } from '@blueriq/core/testing';
+import { BlueriqTestSession } from '@blueriq/angular/testing/src/test_session';
+import { ButtonTemplate, ContainerTemplate, FieldTemplate, TextItemTemplate } from '@blueriq/core/testing';
+import { ElementComponent } from '../../generic/element/element.component';
+import { ButtonComponent } from '../button/button.component';
 import { MaterialModule } from '../material/material.module';
+import { TextItemComponent } from '../textitem/textitem.component';
 import { TableComponent } from './table.component';
+import { TableReadonlyComponent } from './table.readonly.component';
 
-describe('TableComponent', () => {
-  const table = ContainerTemplate.create();
-  let session;
-  let component;
+fdescribe('TableComponent', () => {
+  let tableTemplate: ContainerTemplate;
+  let session: BlueriqTestSession;
+  let component: ComponentFixture<TableComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TableComponent],
-      providers: [BlueriqComponents.register([TableComponent])],
+      declarations: [TableComponent, TableReadonlyComponent, ElementComponent
+        , TextItemComponent, ButtonComponent],
+      providers: [BlueriqComponents.register([TableComponent, TableReadonlyComponent,
+        TextItemComponent, ButtonComponent]), Table],
       imports: [
         MaterialModule,
         BrowserAnimationsModule, // or NoopAnimationsModule
-        BlueriqModule.forRoot(),
+        BlueriqModule.forRoot(), // TODO delete me
         BlueriqTestingModule,
-        FormsModule
-      ]
+        FormsModule,
+      ],
     });
   }));
 
-  beforeEach( () => {
+  beforeEach(() => {
     // Create a ElementComponent  based on a TableComponent.
-    table.contentStyle('table');
-    session = BlueriqSessionTemplate.create().build(table);
+    tableTemplate = ContainerTemplate.create();
+    tableTemplate.contentStyle('table');
+    tableTemplate.children(
+      ///////////// HEADER ////////////
+      ContainerTemplate
+      .create('header')
+      .contentStyle('tablerow')
+      .children(
+        ContainerTemplate
+        .create('cell')
+        .contentStyle('tableheader')
+        .children(
+          TextItemTemplate.create('Name').plainText('Name'), // type: textitem
+          ButtonTemplate.create('sort_%s0').styles('sort', 'descending'),
+        ),
+      ),
+      ///////////// ROW ////////////
+      ContainerTemplate
+      .create('row')
+      .contentStyle('tablerow')
+      .children(
+        ContainerTemplate
+        .create('cell')
+        .contentStyle('tablecell')
+        .children(
+          TextItemTemplate.create('Name2').plainText('Name2'), // type: textitem
+          FieldTemplate.text('column0').value('Mike').readonly(true),
+        ),
+      ),
+    );
+    session = BlueriqSessionTemplate.create().build(tableTemplate);
     component = session.get(TableComponent);
   });
 
   it('should have a filter', () => {
-
-    const selectedElement = component.nativeElement.querySelector('.mat-form-field');
-    expect(selectedElement).toBeTruthy();
+    // component.componentInstance.table.columnNames$.subscribe((e) => console.log(e));
+    // component.componentInstance.table.rows$.subscribe((e) => console.log(e));
+    // component.componentInstance.table.columns$.subscribe((e) => console.log(e));
+    // console.log(component.nativeElement);
+    expect(component).toBeTruthy();
   });
+  // Columns aanwezig in DOM?
+  // Columns aanwezig
 
-  fit('should have a paginator', () => {
+  function getTableHeaderRow() {
+    return getTableRow('header', 'tableheader');
+  }
 
-    const selectedElement = component.nativeElement.querySelector('.mat-paginator');
-    expect(selectedElement).toBeTruthy();
-  });
+  function getTableBodyRow() {
+    return getTableRow('row', 'tablecell');
+  }
+
+  function getTableRow(name, cell) {
+    const tableRow = ContainerTemplate.create(name).contentStyle('tablerow');
+    tableRow.children(getTableCell(0, cell));
+    return tableRow;
+  }
+
+  function getTableCell(index, contentStyle) {
+    const tableCell = ContainerTemplate.create('cell').contentStyle(contentStyle); // or tablesortedheader
+    tableCell.children(
+      TextItemTemplate.create('Name'), // type: textitem
+      ButtonTemplate.create('sort_%s' + index).styles('sort', 'descending'), // type: button
+    );
+    return tableCell;
+  }
+
+  function paste() {
+    ContainerTemplate
+    .create('header')
+    .contentStyle('tablerow')
+    .children(
+      ContainerTemplate
+      .create('cell')
+      .contentStyle('tableheader')
+      .children(
+        TextItemTemplate.create('Name'), // type: textitem
+        ButtonTemplate.create('sort_%s0').styles('sort', 'descending'),
+      ),
+    );
+  }
 
 });
