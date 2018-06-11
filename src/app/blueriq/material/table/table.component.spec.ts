c7c5700e2d92bcf59b1dde531417de64b7cc5bf6
Mike van Emmerik <m.van.emmerik@everest.nl>
2018-06-11 14:21:12 +0200

    test(forms): Test for tables #BQ-4580 \n\n Test for table component

--- src/app/blueriq/material/table/table.component.spec.ts
+++ src/app/blueriq/material/table/table.component.spec.ts
@@ -1,16 +1,14 @@
-///<reference path="../../../../../node_modules/@types/jasmine/index.d.ts"/>
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BlueriqComponents, BlueriqModule } from '@blueriq/angular';
+import { BlueriqComponents } from '@blueriq/angular';
 import { Table } from '@blueriq/angular/lists';
 import { BlueriqSessionTemplate, BlueriqTestingModule } from '@blueriq/angular/testing';
 import { BlueriqTestSession } from '@blueriq/angular/testing/src/test_session';
 import { ButtonTemplate, ContainerTemplate, FieldTemplate, TextItemTemplate } from '@blueriq/core/testing';
-import { ElementComponent } from '../../generic/element/element.component';
-import { ButtonComponent } from '../button/button.component';
 import { MaterialModule } from '../material/material.module';
 import { TextItemComponent } from '../textitem/textitem.component';
+import { TableSortComponent } from './sort/table.sort.component';
 import { TableComponent } from './table.component';
 import { TableReadonlyComponent } from './table.readonly.component';
 
@@ -21,14 +19,16 @@ fdescribe('TableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TableComponent, TableReadonlyComponent, ElementComponent
-        , TextItemComponent, ButtonComponent],
-      providers: [BlueriqComponents.register([TableComponent, TableReadonlyComponent,
-        TextItemComponent, ButtonComponent]), Table],
+      declarations: [TableComponent, TableReadonlyComponent,
+        TableSortComponent, TextItemComponent],
+      providers: [BlueriqComponents.register([
+        TableComponent, TableReadonlyComponent,
+        TableSortComponent, TextItemComponent]),
+        Table,
+      ],
       imports: [
         MaterialModule,
         BrowserAnimationsModule, // or NoopAnimationsModule
-        BlueriqModule.forRoot(), // TODO delete me
         BlueriqTestingModule,
         FormsModule,
       ],
@@ -36,87 +36,61 @@ fdescribe('TableComponent', () => {
   }));
 
   beforeEach(() => {
-    // Create a ElementComponent  based on a TableComponent.
     tableTemplate = ContainerTemplate.create();
     tableTemplate.contentStyle('table');
+    // Simulate a table so the red-cow framework detects this and can be tested on.
     tableTemplate.children(
-      ///////////// HEADER ////////////
+      // ---------- Header ----------
       ContainerTemplate
       .create('header')
       .contentStyle('tablerow')
       .children(
         ContainerTemplate
         .create('cell')
-        .contentStyle('tableheader')
+        .contentStyle('tablesortedheader')
         .children(
-          TextItemTemplate.create('Name').plainText('Name'), // type: textitem
-          ButtonTemplate.create('sort_%s0').styles('sort', 'descending'),
+          TextItemTemplate.create('Name').plainText('Name'),
+          /* 'descending' itself cannot be tested, since this is done by the backend */
+          ButtonTemplate.create().styles('sort', 'descending'),
         ),
       ),
-      ///////////// ROW ////////////
+      // ---------- Row #1 ----------
       ContainerTemplate
       .create('row')
       .contentStyle('tablerow')
       .children(
-        ContainerTemplate
-        .create('cell')
-        .contentStyle('tablecell')
-        .children(
-          TextItemTemplate.create('Name2').plainText('Name2'), // type: textitem
-          FieldTemplate.text('column0').value('Mike').readonly(true),
-        ),
+        FieldTemplate.text('Person.Name').value('Mike').readonly(true),
       ),
+      // ---------- Row #2 ----------
+      ContainerTemplate
+      .create('row')
+      .contentStyle('tablerow')
+      .children(
+        FieldTemplate.text('Person.Name').value('Tilly').readonly(true),
+      ),
+      // ---------- End ----------
     );
     session = BlueriqSessionTemplate.create().build(tableTemplate);
     component = session.get(TableComponent);
   });
 
-  it('should have a filter', () => {
-    // component.componentInstance.table.columnNames$.subscribe((e) => console.log(e));
-    // component.componentInstance.table.rows$.subscribe((e) => console.log(e));
-    // component.componentInstance.table.columns$.subscribe((e) => console.log(e));
-    // console.log(component.nativeElement);
+  it('should have been created', () => {
+    console.log(component.nativeElement);
     expect(component).toBeTruthy();
   });
-  // Columns aanwezig in DOM?
-  // Columns aanwezig
 
-  function getTableHeaderRow() {
-    return getTableRow('header', 'tableheader');
-  }
+  it('should have a header displayed with the correct content', () => {
+    const matRows = component.nativeElement.querySelectorAll('.mat-row');
+    expect(matRows.length).toBe(2);
+    expect(matRows[0].innerHTML).toContain('Mike');
+    expect(matRows[1].innerHTML).toContain('Tilly');
+  });
 
-  function getTableBodyRow() {
-    return getTableRow('row', 'tablecell');
-  }
-
-  function getTableRow(name, cell) {
-    const tableRow = ContainerTemplate.create(name).contentStyle('tablerow');
-    tableRow.children(getTableCell(0, cell));
-    return tableRow;
-  }
-
-  function getTableCell(index, contentStyle) {
-    const tableCell = ContainerTemplate.create('cell').contentStyle(contentStyle); // or tablesortedheader
-    tableCell.children(
-      TextItemTemplate.create('Name'), // type: textitem
-      ButtonTemplate.create('sort_%s' + index).styles('sort', 'descending'), // type: button
-    );
-    return tableCell;
-  }
-
-  function paste() {
-    ContainerTemplate
-    .create('header')
-    .contentStyle('tablerow')
-    .children(
-      ContainerTemplate
-      .create('cell')
-      .contentStyle('tableheader')
-      .children(
-        TextItemTemplate.create('Name'), // type: textitem
-        ButtonTemplate.create('sort_%s0').styles('sort', 'descending'),
-      ),
-    );
-  }
+  it('should have a row with the correct content', () => {
+    const matHeaderCell = component.nativeElement.querySelectorAll('.mat-header-cell');
+    expect(matHeaderCell.length).toBe(1);
+    const headerCellContent = matHeaderCell[0].querySelector('app-textitem').innerHTML;
+    expect(headerCellContent).toContain('Name');
+  });
 
 });
