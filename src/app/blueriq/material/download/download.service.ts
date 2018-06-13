fdd2af6747c2fd14f9ec28bac1398a53c5ba2929
Mike van Emmerik <m.van.emmerik@everest.nl>
2018-06-13 18:04:01 +0200

    fix(Forms): working download url #BQ-4495

--- src/app/blueriq/material/download/download.service.ts
+++ src/app/blueriq/material/download/download.service.ts
@@ -5,18 +5,13 @@ import { Link } from '@blueriq/core';
 @Injectable()
 export class DownloadService {
 
-  constructor(/* inject what?*/) {
-  }
-
-  // ***REMOVED***/api/v2/
-  // session/616b9c36-1cb2-432c-b787-452b169f895e/document/<documentname>/pdf
-  // ?error-redirect=
-  // ***REMOVED***/session/616b9c36-1cb2-432c-b787-452b169f895e/mvc/index.html
   getDownloadUrl(link: Link, blueriqSession: BlueriqSession): string {
-    let url = ' ***REMOVED***/api/v2/';
+    let url = 'http://localhost:4200/Runtime/api/v2/';
     url += 'session/' + blueriqSession.sessionId + '/document/' + link.documentName + '/' + link.documentType;
-    url += '?error-redirect=http://www.google.nl';
-    console.log('generated url:', url);
+    // url += '?error-redirect=';
+    // url += '***REMOVED***/
+    // session/616b9c36-1cb2-432c-b787-452b169f895e/mvc/index.html';
+    // console.log(url);
     return url;
   }
 
