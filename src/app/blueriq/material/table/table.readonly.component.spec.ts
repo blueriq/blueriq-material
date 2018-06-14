eebf60f20a76aaa7ad0ec5d1b286d8618456fa51
r.de.haard <r.de.haard@everest.nl>
2018-06-14 07:56:43 +0200

    test(forms): refactor tests table.readonly.component #BQ-4580

--- src/app/blueriq/material/table/table.readonly.component.spec.ts
+++ src/app/blueriq/material/table/table.readonly.component.spec.ts
@@ -1,47 +1,33 @@
-import { Domain, Field } from '@blueriq/core';
-import { instance, mock, when } from 'ts-mockito';
+import { FieldTemplate } from '@blueriq/core/testing';
 import { TableReadonlyComponent } from './table.readonly.component';
 
 describe('TableComponent', () => {
 
   it('should return domain display value', () => {
-    // Setup mock
-    const field: Field<String> = mock(Field);
-    when(field.getValue()).thenReturn('f');
-    const fieldInstance: Field = instance(field) as Field;
-    fieldInstance.hasDomain = true;
-    fieldInstance.domain = createDomain();
+    const field = FieldTemplate.text('')
+      .value('f')
+      .domain({
+        'f': 'Female',
+        'm': 'Male'
+      }).build();
 
     // SUT
-    const component = new TableReadonlyComponent(fieldInstance);
+    const component = new TableReadonlyComponent(field);
 
     // Verify
     expect( component.getDisplayValue() ).toBe( 'Female' );
   });
 
   it('should return displayValue', () => {
-    // Setup mock
-    const fieldMock: Field<String> = mock(Field);
-    when(fieldMock.getValue()).thenReturn('Female');
-    const fieldInstance: Field = instance(fieldMock) as Field;
-    fieldInstance.hasDomain = false;
+    const field = FieldTemplate.text('')
+      .value('Female')
+      .build();
 
     // SUT
-    const component = new TableReadonlyComponent(fieldInstance);
+    const component = new TableReadonlyComponent(field);
 
     // Verify
     expect( component.getDisplayValue() ).toBe( 'Female' );
   });
 
-  function createDomain() {
-    return new Domain([{
-        value: 'f',
-        displayValue: 'Female'
-      },
-        {
-          value: 'm',
-          displayValue: 'Male'
-        }]
-    );
-  }
 });
