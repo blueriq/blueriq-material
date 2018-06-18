3c6f07587fd4c3215e9489c5daac02f7ffca3540
Felix Janssen <f.janssen@everest.nl>
2018-06-18 13:40:55 +0200

    style(elements): style danger, info, success and warning textitem with icons #BQ-4629

--- src/app/blueriq/material/textitem/textitem.component.ts
+++ src/app/blueriq/material/textitem/textitem.component.ts
@@ -1,6 +1,7 @@
 import { Component, Host } from '@angular/core';
 import { BlueriqComponent } from '@blueriq/angular';
 import { TextItem } from '@blueriq/core';
+import { PresentationStyles } from '../presentationstyles/presentationstyles';
 
 @Component({
   templateUrl: './textitem.component.html',
@@ -14,6 +15,26 @@ import { TextItem } from '@blueriq/core';
 export class TextItemComponent {
 
   constructor(@Host() public textItem: TextItem) {
+  }
+
+  public shouldDisplayGutter(): boolean {
+    return this.textItem.styles.has(PresentationStyles.DANGER) ||
+      this.textItem.styles.has(PresentationStyles.WARNING) ||
+      this.textItem.styles.has(PresentationStyles.INFO) ||
+      this.textItem.styles.has(PresentationStyles.SUCCESS);
+  }
+
+  public getGutterIcon(): string {
+    if (this.textItem.styles.has(PresentationStyles.DANGER)) {
+      return 'icon-danger';
+    } else if (this.textItem.styles.has(PresentationStyles.WARNING)) {
+      return 'icon-warning';
+    } else if (this.textItem.styles.has(PresentationStyles.INFO)) {
+      return 'icon-info';
+    } else if (this.textItem.styles.has(PresentationStyles.SUCCESS)) {
+      return 'icon-success';
+    }
+    return '';
   }
 
 }
