48b2424d554714774768029d4ff377dd832510bb
r.de.haard <r.de.haard@everest.nl>
2018-06-25 08:51:41 +0200

    Revert "build(qc) debug reporting plugin #BQ-4647"
    This reverts commit 90ee738cf30d3762770e9e46b59ae4a4a4cb6a53.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -51,7 +51,7 @@ module.exports = function (config) {
       combineBrowserReports: true,
       verbose: false, // set true for debugging purpose
       reports: [
-        'html','cobertura'
+        'cobertura'
       ], //echo error %errorlevel%
       skipFilesWithNoCoverage: false,
       thresholds: {
