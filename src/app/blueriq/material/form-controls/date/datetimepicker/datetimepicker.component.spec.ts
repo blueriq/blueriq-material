1ab07d873fbe0f251e9e8ab2fb89a31b676c8c1f
r.de.haard <r.de.haard@everest.nl>
2018-06-26 13:34:21 +0200

    fix(coverage): code coverage for datetime picker: format value on change test (WIP) #BQ-4647

--- src/app/blueriq/material/form-controls/date/datetimepicker/datetimepicker.component.spec.ts
+++ src/app/blueriq/material/form-controls/date/datetimepicker/datetimepicker.component.spec.ts
@@ -1,10 +1,10 @@
+import { Input, Input } from "@angular/core";
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BlueriqComponents } from '@blueriq/angular';
 import { BlueriqSessionTemplate, BlueriqTestingModule, BlueriqTestSession } from '@blueriq/angular/testing';
 import { FieldTemplate } from '@blueriq/core/testing';
-import * as moment from 'moment';
 import { OwlDateTimeModule } from 'ng-pick-datetime';
 import { MomentDateTimeAdapter, OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
 import { ElementComponent } from '../../../../generic/element/element.component';
@@ -12,6 +12,7 @@ import { MaterialModule } from '../../../material.module';
 import { PresentationStyles } from '../../../presentationstyles/presentationstyles';
 import { MomentTransformer } from '../moment-transformer';
 import { DateTimepickerComponent } from './datetimepicker.component';
+import moment = require("moment");
 
 describe('DateTimepickerComponent', () => {
   let field: FieldTemplate;
@@ -93,4 +94,24 @@ describe('DateTimepickerComponent', () => {
     spyOn(MomentDateTimeAdapter.prototype, 'now').and.throwError('error');
     expect(component.componentInstance.getFirstDayOfWeek()).toEqual(1);
   });
+
+  it('should format on change', () => {
+    session = BlueriqSessionTemplate.create().build(field);
+    component = session.get(DateTimepickerComponent);
+
+    let inputField = component.nativeElement.querySelector('input');
+    sendInput(inputField, '18-03-03');
+
+     const value = component.nativeElement.querySelector('input');
+     expect(value.value).toBe('2018-01-01');
+  });
+
+  function sendInput(inputField, text: string) {
+    inputField.value = text;
+    const event = new Event('dateTimeChange');
+    inputField.dispatchEvent(event);
+
+    component.detectChanges();
+    return component.whenStable();
+  }
 });
