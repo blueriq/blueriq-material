994be91e270aeb90368f2c444910c8bb817f090e
Marielle van Verseveld <m.v.verseveld-boers@everest.nl>
2018-06-29 10:48:10 +0200

    feat(justification) added asset component #BQ-4496
    added AssetComponent to show the justifications

---
+++ src/app/blueriq/material/asset/asset.component.spec.ts
@@ -0,0 +1,43 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlueriqComponents } from '@blueriq/angular';
+import { BlueriqSessionTemplate, BlueriqTestingModule, BlueriqTestSession } from '@blueriq/angular/testing';
+import { TextItemModule } from '@blueriq/angular/textitems';
+import { AssetTemplate } from '@blueriq/core/testing';
+import { MaterialModule } from '../material.module';
+
+import { AssetComponent } from './asset.component';
+
+describe('AssetComponent', () => {
+  let assetTemplate: AssetTemplate;
+  let component: ComponentFixture<AssetComponent>;
+  let session: BlueriqTestSession;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AssetComponent],
+      providers: [BlueriqComponents.register([AssetComponent])],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        BlueriqTestingModule,
+        TextItemModule
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    assetTemplate = AssetTemplate.create();
+    session = BlueriqSessionTemplate.create().build(assetTemplate);
+    component = session.get(AssetComponent);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contain text', () => {
+    const inputAsset = 'lorum ipsum';
+    expect(inputAsset).toBe('lorum ipsum');
+  });
+});
