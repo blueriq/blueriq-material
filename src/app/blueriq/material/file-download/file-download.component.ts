cb7d0243236aac40311d52badc288f370e62b3c0
Geert Graat <g.graat@everest.nl>
2018-07-02 17:07:48 +0200

    rework(file-handling): moved download component functionality to RedCow (FileDownloadContainer), use that from now

--- src/app/blueriq/material/file-download/file-download.component.ts
+++ src/app/blueriq/material/file-download/file-download.component.ts
@@ -1,38 +1,20 @@
-import { Component, Host } from '@angular/core';
-import { BlueriqComponent, BlueriqSession, DownloadInfo, DownloadService } from '@blueriq/angular';
-import { Button, Container } from '@blueriq/core';
+import { Component, Self } from '@angular/core';
+import { BlueriqComponent, FileDownloadContainer } from '@blueriq/angular';
+import { Container } from '@blueriq/core';
 
 @Component({
   selector: 'bq-file-download',
   templateUrl: './file-download.component.html',
-  styleUrls: ['./file-download.component.scss']
+  styleUrls: ['./file-download.component.scss'],
+  providers: [FileDownloadContainer]
 })
 @BlueriqComponent({
   type: Container,
-  selector: '[contentStyle=filedownload]'
+  selector: 'filedownload'
 })
 export class FileDownloadComponent {
 
-  downloadButton: Button;
-  showError: string;
-
-  constructor(@Host() private container: Container, private readonly session: BlueriqSession,
-              private readonly downloadService: DownloadService) {
-    this.downloadButton = this.container.children[0] as Button;
+  constructor(@Self() private container: FileDownloadContainer) {
   }
 
-  download(): void {
-    this.downloadService.getDownloadInfo(this.session.sessionId,
-      this.container.properties.configurationid).subscribe((data: DownloadInfo) => {
-      if (data.type === 'authorized') {
-        window.location.href = data.url;
-      } else if (data.type === 'unauthorized') {
-        this.showError = 'UNAUTHORIZED';
-      } else if (data.type === 'error') {
-        this.showError = data.errorMessage;
-      } else {
-        this.showError = 'WHAT HAPPENED?';
-      }
-    }, error => console.error(error), () => console.log('completed'));
-  }
 }
