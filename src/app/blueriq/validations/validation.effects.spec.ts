72f559a46ffbe09844a552e8b7274efd457aaf6e
Marielle van Verseveld <m.v.verseveld-boers@everest.nl>
2018-07-03 13:35:31 +0200

    test(snackbar) added testcases #BQ-4630

--- src/app/blueriq/validations/validation.effects.spec.ts
+++ src/app/blueriq/validations/validation.effects.spec.ts
@@ -3,7 +3,7 @@ import { MatSnackBar } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonPressHandledAction } from '@blueriq/angular';
 import { BlueriqSessionTemplate, BlueriqTestingModule } from '@blueriq/angular/testing';
-import { Button } from '@blueriq/core';
+import { Button, Field } from '@blueriq/core';
 import { ButtonTemplate, ContainerTemplate, FieldTemplate } from '@blueriq/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Subject } from 'rxjs/Subject';
@@ -71,5 +71,58 @@ describe('ValidationsComponent', () => {
     expect(snackBarSpy.open).toHaveBeenCalledWith('There are validation warnings on the page', undefined, {
       duration: 5000
     });
+  });
+
+  it('should not open a snackbar because there is no validation', () => {
+    let buttonElement: Button;
+    const container = ContainerTemplate.create().children(
+      ButtonTemplate.create().validate(true).assign(el => buttonElement = el)
+    );
+
+    BlueriqSessionTemplate.create().build(container);
+
+    const action = new ButtonPressHandledAction('session-name', buttonElement!.functionalKey);
+
+    effects.buttonHandled$.subscribe();
+
+    actions.next(action);
+
+    expect(snackBarSpy.open).not.toHaveBeenCalled();
+  });
+
+  it('should not open a snackbar because the button does not has validate', () => {
+    let buttonElement: Button;
+    const container = ContainerTemplate.create().children(
+      FieldTemplate.text().warning('This is a warning'),
+      ButtonTemplate.create().validate(false).assign(el => buttonElement = el)
+    );
+
+    BlueriqSessionTemplate.create().build(container);
+
+    const action = new ButtonPressHandledAction('session-name', buttonElement!.functionalKey);
+
+    effects.buttonHandled$.subscribe();
+
+    actions.next(action);
+
+    expect(snackBarSpy.open).not.toHaveBeenCalledWith();
+  });
+
+  it('should not open a snackbar because the action element was not a button', () => {
+    let fieldElement: Field;
+    const container = ContainerTemplate.create().children(
+      FieldTemplate.text().warning('This is a warning'),
+      FieldTemplate.text().assign(el => fieldElement = el)
+    );
+
+    BlueriqSessionTemplate.create().build(container);
+
+    const action = new ButtonPressHandledAction('session-name', fieldElement!.functionalKey);
+
+    effects.buttonHandled$.subscribe();
+
+    actions.next(action);
+
+    expect(snackBarSpy.open).not.toHaveBeenCalledWith();
   });
 });
