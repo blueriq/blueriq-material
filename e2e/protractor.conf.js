b023d3e8482e6667fe42e8a96ff387f9f8108532
r.de.haard <r.de.haard@everest.nl>
2018-07-17 09:54:18 +0200

    test(e2e): update e2e: try without different reporters per browser #BQ-4686

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -40,21 +40,14 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
-
-    // returning the promise makes protractor wait for the reporter config before executing tests
-    return browser.getProcessedConfig().then(function(config) {
-      // you could use other properties here if you want, such as platform and version
-      var browserName = config.capabilities.browserName;
-
       jasmine.getEnv().addReporter(
         new Jasmine2HtmlReporter({
           savePath: 'testresults',
           screenshotsFolder: 'images',
           takeScreenshots: true,
           takeScreenshotsOnlyOnFailures: true,
-          fileNamePrefix: browserName + '-results'
+          fileNamePrefix: 'test' + '-results'
         })
       );
-    });
   }
 };
