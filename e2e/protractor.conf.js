093823dd0fc242c9ea51563084044109064fb87f
r.de.haard <r.de.haard@everest.nl>
2018-07-17 10:48:53 +0200

    test(e2e): update e2e: try to publish reporters per browser #BQ-4686

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,7 +2,6 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const {SpecReporter} = require('jasmine-spec-reporter');
-const Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -41,14 +40,20 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
-      jasmine.getEnv().addReporter(
+
+    // returning the promise makes protractor wait for the reporter config before executing tests
+    return browser.getProcessedConfig().then(function(config) {
+      // you could use other properties here if you want, such as platform and version
+      var browserName = config.capabilities.browserName;
+
         new Jasmine2HtmlReporter({
           savePath: 'testresults',
           screenshotsFolder: 'images',
           takeScreenshots: true,
           takeScreenshotsOnlyOnFailures: true,
-          fileNamePrefix: 'test' + '-results'
+          fileNamePrefix: browserName + '-results'
         })
       );
+    });
   }
 };
