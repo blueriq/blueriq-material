04d6df40ba421e8629af40d6a2795243b2e4575a
r.de.haard <r.de.haard@everest.nl>
2018-07-17 08:47:29 +0200

    test(e2e): update e2e: added junit test results per browser #BQ-4686

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -39,6 +39,26 @@ exports.config = {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    require('jasmine-reporters');
+    //jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+
+    // returning the promise makes protractor wait for the reporter config before executing tests
+    return browser.getProcessedConfig().then(function(config) {
+      // you could use other properties here if you want, such as platform and version
+      var browserName = config.capabilities.browserName;
+
+      var junitReporter = new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: 'testresults',
+        // this will produce distinct xml files for each capability
+        filePrefix: browserName + '-xmloutput',
+        modifySuiteName: function(generatedSuiteName, suite) {
+          // this will produce distinct suite names for each capability,
+          // e.g. 'firefox.login tests' and 'chrome.login tests'
+          return browserName + '.' + generatedSuiteName;
+        }
+      });
+      jasmine.getEnv().addReporter(junitReporter);
+    });
   }
 };
