396f4042d1bcf1708a129b06bd949fdc36f47b30
r.de.haard <r.de.haard@everest.nl>
2018-07-17 10:32:01 +0200

    test(e2e): update e2e: try without different reporters per browser #BQ-4686

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const {SpecReporter} = require('jasmine-spec-reporter');
+const {Jasmine2HtmlReporter} = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
