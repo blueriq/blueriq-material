acddff32fae5469d32eefa8ff0c4fd2299586abf
r.de.haard <r.de.haard@everest.nl>
2018-07-17 09:41:16 +0200

    test(e2e): update e2e: try different html reporter #BQ-4686

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -46,18 +46,15 @@ exports.config = {
       // you could use other properties here if you want, such as platform and version
       var browserName = config.capabilities.browserName;
 
-      var junitReporter = new jasmineReporters.JUnitXmlReporter({
-        consolidateAll: true,
-        savePath: 'testresults',
-        // this will produce distinct xml files for each capability
-        filePrefix: browserName + '-xmloutput',
-        modifySuiteName: function(generatedSuiteName, suite) {
-          // this will produce distinct suite names for each capability,
-          // e.g. 'firefox.login tests' and 'chrome.login tests'
-          return browserName + '.' + generatedSuiteName;
-        }
-      });
-      jasmine.getEnv().addReporter(junitReporter);
+      jasmine.getEnv().addReporter(
+        new Jasmine2HtmlReporter({
+          savePath: 'testresults',
+          screenshotsFolder: 'images',
+          takeScreenshots: true,
+          takeScreenshotsOnlyOnFailures: true,
+          fileNamePrefix: browserName + '-results'
+        })
+      );
     });
   }
 };
