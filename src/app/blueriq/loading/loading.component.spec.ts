eff9a7cf6453bae48b713e91cd0de14f4e838b0e
Marielle van Verseveld <m.v.verseveld-boers@everest.nl>
2018-07-17 15:00:39 +0200

    test(loading-spinner) add testcases for loading-spinner #BQ-4627

--- src/app/blueriq/loading/loading.component.spec.ts
+++ src/app/blueriq/loading/loading.component.spec.ts
@@ -1,4 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormattingModule } from '@blueriq/angular/formatting';
+import { BlueriqTestingModule } from '@blueriq/angular/testing';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { MaterialModule } from '../material/material.module';
 
 import { LoadingComponent } from './loading.component';
 
@@ -8,9 +14,15 @@ describe('LoadingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LoadingComponent]
-    })
-    .compileComponents();
+      declarations: [LoadingComponent],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule, // or NoopAnimationsModule
+        BlueriqTestingModule,
+        FormsModule,
+        FormattingModule.forRoot()
+      ]
+    });
   }));
 
   beforeEach(() => {
@@ -21,5 +33,29 @@ describe('LoadingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('function ngOnDestroy should be complete and unsubscribe', () => {
+    // Init
+    spyOn(BehaviorSubject.prototype, 'complete');
+    spyOn(BehaviorSubject.prototype, 'unsubscribe');
+
+    // Sut
+    component.ngOnDestroy();
+
+    // Verify
+    expect(BehaviorSubject.prototype.complete).toHaveBeenCalled();
+    expect(BehaviorSubject.prototype.unsubscribe).toHaveBeenCalled();
+  });
+
+  it('function onInitialize should be done starting', () => {
+    // Init
+    spyOn(BehaviorSubject.prototype, 'next');
+
+    // Sut
+    component.onInitialize();
+
+    // Verify
+    expect(BehaviorSubject.prototype.next).toHaveBeenCalledWith(false);
   });
 });
