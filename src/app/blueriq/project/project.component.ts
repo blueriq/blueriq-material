80a3de08063d8e374034bf6d294fc33a37ce3f71
Felix Janssen <f.janssen@everest.nl>
2018-07-18 16:48:39 +0200

    feat(error-message): Add a first version of the message display through event emitters #BQ-4504

--- src/app/blueriq/project/project.component.ts
+++ src/app/blueriq/project/project.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProjectDetails } from '@blueriq/angular';
 import { SessionId } from '@blueriq/core';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
+import { ErrorService } from '../error/error.service';
 
 @Component({
-  templateUrl: './project.component.html',
+  templateUrl: './project.component.html'
 })
 export class ProjectComponent implements OnInit {
 
@@ -16,7 +18,8 @@ export class ProjectComponent implements OnInit {
   flow: Observable<string>;
   languageCode: Observable<string>;
 
-  constructor(private readonly route: ActivatedRoute) {
+  constructor(private readonly route: ActivatedRoute,
+              private errorService: ErrorService) {
   }
 
   get sessionName(): string {
@@ -33,6 +36,30 @@ export class ProjectComponent implements OnInit {
     this.project = this.route.paramMap.pipe(map(params => params.get('project') || ''));
     this.flow = this.route.paramMap.pipe(map(params => params.get('flow') || ''));
     this.languageCode = this.route.paramMap.pipe(map(params => params.get('languageCode') || ''));
+  }
+
+  onSessionExpired() {
+    this.errorService.emitError({
+      errorType: 'SESSION_EXPIRED',
+      title: 'Session expired',
+      message: 'Your session has expired'
+    });
+  }
+
+  onFlowEnded() {
+    this.errorService.emitError({
+      errorType: 'FLOW_ENDED',
+      title: 'Flow ended',
+      message: 'The flow has ended'
+    });
+  }
+
+  onUnauthorized(details: ProjectDetails) {
+    this.errorService.emitError({
+      errorType: 'UNAUTHORIZED',
+      title: 'Unauthorized',
+      message: 'You are not authorized to ' + details.resource
+    });
   }
 }
 
