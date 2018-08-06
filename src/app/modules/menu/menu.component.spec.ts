56fca92cabfc77dc87bc2a1bba4bc003cb5b2306
Geert Graat <g.graat@everest.nl>
2018-08-06 17:40:25 +0200

    test(composite-menu): provided unittests for submenu's #BQ-4501

--- src/app/modules/menu/menu.component.spec.ts
+++ src/app/modules/menu/menu.component.spec.ts
@@ -1,25 +1,15 @@
-import { ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatMenuTrigger } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BlueriqSession } from '@blueriq/angular';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BlueriqSessionTemplate, BlueriqTestingModule, BlueriqTestSession } from '@blueriq/angular/testing';
 import { ButtonTemplate, ContainerTemplate } from '@blueriq/core/testing';
 import { MenuComponent } from './menu.component';
 import { MenuModule } from './menu.module';
 
-export class TestTrigger {
-  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-
-  open() {
-    this.trigger.openMenu();
-  }
-}
-
 describe('MenuComponent', () => {
     let menu: ContainerTemplate;
-    let unitMenu: ContainerTemplate;
     let component: ComponentFixture<MenuComponent>;
     let session: BlueriqTestSession;
 
@@ -28,7 +18,7 @@ describe('MenuComponent', () => {
         // declarations: [ButtonComponent],
         imports: [
           MenuModule,
-          BrowserAnimationsModule, // or NoopAnimationsModule
+          NoopAnimationsModule,
           BlueriqTestingModule,
           FormsModule
         ]
@@ -37,15 +27,18 @@ describe('MenuComponent', () => {
 
     beforeEach(() => {
       menu = ContainerTemplate.create().contentStyle('dashboard_menu');
-      unitMenu = ContainerTemplate.create();
 
       menu.children(
         ContainerTemplate.create().contentStyle('menubar').children(
           ButtonTemplate.create('Home').caption('Home'),
-          unitMenu.displayName('Unit').children(
+          ContainerTemplate.create().displayName('Unit').children(
             ButtonTemplate.create('Core').caption('Core'),
             ButtonTemplate.create('Finance').caption('Finance'),
-            ButtonTemplate.create('Public').caption('Public'))
+            ContainerTemplate.create().displayName('Public').children(
+              ButtonTemplate.create('Public').caption('Public-A'),
+              ButtonTemplate.create('Public').caption('Public-B')
+            )
+          )
         )
       );
       // reset field to default
@@ -57,20 +50,45 @@ describe('MenuComponent', () => {
       expect(component).toBeTruthy();
     });
 
-    // test 1: dat buttons onClick event aangeroepen wordt EN een spy dat session.pressed aangeroepen wordt.
-    // zo kunnen we garanderen dat navigeren werkt
-    fit('should call the session when it gets clicked', () => {
-      spyOn(BlueriqSession.prototype, 'pressed');
-      TestTrigger.prototype.open();
-      // expect(BlueriqSession.prototype.pressed).not.toHaveBeenCalled();
-      expect(BlueriqSession.prototype.pressed).toHaveBeenCalledTimes(1);
+    it('should display submenus when the menu button is clicked', () => {
+      // by default, no submenus are shown
+      const subMenu = component.debugElement.query(By.css('.mat-menu-content'));
+      expect(subMenu).toBeFalsy();
 
+      // retrieve the trigger
+      const selectTrigger = component.debugElement.query(By.directive(MatMenuTrigger));
+      expect(selectTrigger).toBeTruthy();
+
+      // click on the menu button (via the trigger) to display the submenu
+      selectTrigger.nativeElement.click();
+
+      component.whenStable()
+      .then(() => {
+        component.detectChanges();
+        const subMenu = component.debugElement.query(By.css('.mat-menu-content')).nativeElement;
+        const menuOptions = subMenu.querySelectorAll('bq-menu-item') as NodeListOf<HTMLElement>;
+
+        // verify
+        expect(menuOptions.length).toBe(3);
+        expect(menuOptions[0].innerText.trim()).toBe('CORE');
+        expect(menuOptions[1].innerText.trim()).toBe('FINANCE');
+        expect(menuOptions[2].innerText.trim()).toBe('PUBLIC');
+
+        // expand the following submenu by clicking the 'Public' button
+        menuOptions[2].getElementsByTagName('button')[0].click();
+        component.whenStable()
+        .then(() => {
+          component.detectChanges();
+          // now 2 mat-menu-content sections exist, we want to verify the last one with the sub-submenu
+          const subMenu = component.debugElement.queryAll(By.css('.mat-menu-content'))[1].nativeElement;
+          const menuOptions = subMenu.querySelectorAll('bq-menu-item') as NodeListOf<HTMLElement>;
+
+          // verify
+          expect(menuOptions.length).toBe(2);
+          expect(menuOptions[0].innerText.trim()).toBe('PUBLIC-A');
+          expect(menuOptions[1].innerText.trim()).toBe('PUBLIC-B');
+        });
+      });
     });
   }
-
-// test 2: dat een container met knoppen en daarin weer containers uiteindelijk weer gerenderd wordt,
-// ze test je de recursie
-
-// test 3: simpele test, alle items zijn gerenderd. (A | B | C | D->D1 )
-
 );
