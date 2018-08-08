7c5297d4e713340c1daf4080cdc7914620c6c1b2
m.van.emmerik <m.van.emmerik@everest.nl>
2018-08-08 17:26:27 +0200

    refactor(file-download): Make filedownload use the button as way of rendering #BQ-4498

--- src/app/modules/file/file-download/file-download.component.ts
+++ src/app/modules/file/file-download/file-download.component.ts
@@ -1,27 +1,35 @@
-import { Component, Self } from '@angular/core';
-import { AuthorizedDownload, BlueriqComponent } from '@blueriq/angular';
+import { Component, Host, Optional, Self, ViewEncapsulation } from '@angular/core';
+import { AuthorizedDownload, BlueriqComponent, BlueriqSession } from '@blueriq/angular';
 import { FileDownload } from '@blueriq/angular/files';
+import { Table } from '@blueriq/angular/lists';
 import { Container } from '@blueriq/core';
+import { ButtonComponent } from '../../button/button.component';
 import { FileDownloadService } from './file-download.service';
 
 @Component({
   selector: 'bq-file-download',
-  templateUrl: './file-download.component.html',
-  styleUrls: ['./file-download.component.scss'],
-  providers: [FileDownload]
+  templateUrl: '../../button/button.component.html',
+  styleUrls: ['../../button/button.component.scss'],
+  providers: [FileDownload],
+  encapsulation: ViewEncapsulation.None
 })
 @BlueriqComponent({
   type: Container,
   selector: 'filedownload'
 })
-export class FileDownloadComponent {
+export class FileDownloadComponent extends ButtonComponent {
 
   constructor(@Self() public fileDownload: FileDownload,
+              session: BlueriqSession,
+              @Optional() @Host() public readonly table: Table,
               private fileDownloadService: FileDownloadService) {
+    super(fileDownload.downloadButton, session, table);
   }
 
-  download(): void {
+  /* Overrides */
+  onClick(): void {
     this.fileDownload.getDownloadInfo().subscribe((downloadInfo: AuthorizedDownload) => {
+      console.log(downloadInfo);
       this.fileDownloadService.download(downloadInfo.url);
     });
   }
