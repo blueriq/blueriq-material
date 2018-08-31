3cb95ff9e52442def7b065be2372b758b8993e64
Felix Janssen <f.janssen@everest.nl>
2018-08-31 10:35:32 +0200

    feat(flow-widget): also handle expired and flowEnded events #BQ-5026

--- src/app/modules/widget/flow-widget.component.ts
+++ src/app/modules/widget/flow-widget.component.ts
@@ -14,12 +14,36 @@ import { Container } from '@blueriq/core';
 export class FlowWidgetComponent {
 
   bqError: FailedAction;
+  private expired = false;
+  private flowEnded = false;
 
-  constructor(@Host() public widget: Container,
+  constructor(@Host() public widgetContainer: Container,
               @Self() public flowWidget: FlowWidget) {
   }
 
-  getErrorMessage(): string {
+  get errorMessage(): string {
+    if (this.expired) {
+      return 'Your session has expired';
+    } else if (this.flowEnded) {
+      return 'The flow has ended';
+    }
     return this.bqError.error.cause.message;
+  }
+
+  shouldDisplayError(): boolean {
+    return this.expired || this.flowEnded || !!this.bqError;
+  }
+
+  handleError(error: FailedAction): void {
+    this.bqError = error;
+  }
+
+  handleFlowEnded(): void {
+    this.flowEnded = true;
+  }
+
+  handleSessionExpired(): void {
+    this.expired = true;
+
   }
 }
