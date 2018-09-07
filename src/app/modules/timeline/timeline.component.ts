81b23f5fa785ce5f722fc6cd4ca5f4a9e5351688
m.van.emmerik <m.van.emmerik@everest.nl>
2018-09-07 14:24:47 +0200

    feat(timeline): Implementation of the timeline #BQ-4499

--- src/app/modules/timeline/timeline.component.ts
+++ src/app/modules/timeline/timeline.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { BlueriqChildren, BlueriqComponent } from '@blueriq/angular';
+import { BlueriqComponent, BlueriqSession, DashboardTimeline } from '@blueriq/angular';
 import { Container } from '@blueriq/core';
+import * as moment from 'moment';
+import { parseBqDateTimePattern, parseBqLocale } from '../../configuration/date/bq-date-parser';
 
 @Component({
   selector: 'bq-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: ['./timeline.component.scss'],
+  providers: [DashboardTimeline]
 })
 @BlueriqComponent({
   type: Container,
@@ -13,11 +16,21 @@ import { Container } from '@blueriq/core';
 })
 export class TimelineComponent {
 
-  @BlueriqChildren(Container, 'timelineEntry')
-  entries: Container;
+  constructor(public readonly timeline: DashboardTimeline, private readonly session: BlueriqSession) {
+  }
 
-  constructor() {
+  dateToReadableFormat(date: Date): string {
+    moment.locale(parseBqLocale(this.session));
+    const bqDateTime = parseBqDateTimePattern(this.session);
+    const mdate = moment(date, bqDateTime.dateTimePattern);
+    if (moment().diff(mdate, 'days') >= 7) {
+      return mdate.format('LLL');
+    }
+    return mdate.fromNow(false);
+  }
 
+  dateToTimestamp(date: Date): string {
+    return moment(date).format('HH:mm');
   }
 
 }
