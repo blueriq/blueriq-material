82e3e2a9dad0727e49ff1513ed2a8391c65e9993
Felix Janssen <f.janssen@everest.nl>
2018-09-10 13:24:58 +0200

    refactor: refactored flexchild directive to only update when something changes #BQ-4505

--- src/app/modules/container/horizontal-flex-child.directive.ts
+++ src/app/modules/container/horizontal-flex-child.directive.ts
@@ -1,6 +1,7 @@
-import { Directive, DoCheck, Input, Renderer2 } from '@angular/core';
-import { getAngularComponent } from '@blueriq/angular';
+import { Directive, Input, OnDestroy, Renderer2 } from '@angular/core';
+import { BlueriqListeners, getAngularComponent } from '@blueriq/angular';
 import { Container, Element } from '@blueriq/core';
+import { Subscription } from 'rxjs';
 import { BqContentStyles } from '../BqContentStyles';
 import { BqPresentationStyles } from '../BqPresentationStyles';
 
@@ -11,44 +12,65 @@ import { BqPresentationStyles } from '../BqPresentationStyles';
 @Directive({
   selector: '[bqFlexChild]'
 })
-export class HorizontalFlexChildDirective implements DoCheck {
+export class HorizontalFlexChildDirective implements OnDestroy {
 
   private static REGEXP_PS = new RegExp('^' + BqPresentationStyles.WEIGHT_PREFIX + '(\\d+)$');
   private static REGEXP_CS = new RegExp('^' + BqContentStyles.DASHBOARD_COLUMN_PREFIX + '(\\d+)$');
 
-  @Input()
-  bqElement: Element;
+  private _subscription: Subscription | undefined;
 
-  constructor(private renderer: Renderer2) {
+  constructor(private renderer: Renderer2,
+              private listeners: BlueriqListeners) {
   }
 
-  ngDoCheck(): void {
-    if (this.bqElement instanceof Container) {
-      let matches: RegExpMatchArray | null;
-      const presentationStyle = this.bqElement.styles
-      .get(style => style.startsWith(BqPresentationStyles.WEIGHT_PREFIX));
-      if (presentationStyle) {
-        matches = presentationStyle.match(HorizontalFlexChildDirective.REGEXP_PS);
-        if (matches) {
-          this.decorateHostElement(+matches[1]);
+  @Input()
+  set bqElement(element: Element) {
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+    }
+
+    this._subscription = this.listeners.listen(element).subscribe(() => {
+      this.applyStylesIfRequired(element);
+    });
+
+    this.applyStylesIfRequired(element);
+  }
+
+  ngOnDestroy() {
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+    }
+  }
+
+  private applyStylesIfRequired(element: Element): void {
+    if (element instanceof Container) {
+
+      const weightStyle = element.styles.get(style => style.startsWith(BqPresentationStyles.WEIGHT_PREFIX));
+      if (weightStyle) {
+        if (this.decorateHostElement(element, weightStyle, HorizontalFlexChildDirective.REGEXP_PS)) {
           return;
         }
       }
 
-      // (legacy) content styles
-      if (this.bqElement.contentStyle) {
-        matches = this.bqElement.contentStyle.match(HorizontalFlexChildDirective.REGEXP_CS);
-        if (matches) {
-          this.decorateHostElement(+matches[1]);
-          return;
-        }
+      // (legacy) content style
+      if (element.contentStyle) {
+        this.decorateHostElement(element, element.contentStyle, HorizontalFlexChildDirective.REGEXP_CS);
       }
     }
   }
 
-  private decorateHostElement(flexGrow: number) {
-    // retrieve the host element from from the angular component
-    const component = getAngularComponent(this.bqElement);
+  private decorateHostElement(element: Element, style: string, regex: RegExp): boolean {
+    const matches = style.match(regex);
+    if (matches) {
+      this.applyStyles(element, +matches[1]);
+      return true;
+    }
+    return false;
+  }
+
+  private applyStyles(element: Element, flexGrow: number) {
+    // retrieve the host element from the angular component
+    const component = getAngularComponent(element);
     if (component) {
       this.renderer.addClass(component.location.nativeElement, 'column-wrapper');
       this.renderer.setStyle(component.location.nativeElement, 'flex-grow', flexGrow);
