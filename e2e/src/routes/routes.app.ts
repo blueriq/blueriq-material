1fdaa75cac52c5b890b1c9ca5af47c0618247446
r.de.haard <r.de.haard@everest.nl>
2018-09-19 11:34:03 +0200

    tests(project routes) added e2e tests for Blueriq Project routes

---
+++ e2e/src/routes/routes.app.ts
@@ -0,0 +1,31 @@
+import Promise = promise.Promise;
+import { browser, by, element, promise, protractor } from 'protractor';
+
+export class RoutesApp {
+  public PAGE_ID_AANVRAGEN_KINDERBIJSLAG: string = 'P960';
+  public PAGE_ID_DEFAULT_SHORTCUT_START: string = 'P866';
+
+  get pageTitleAanvragenKinderbijslag(): Promise<string> {
+    return this.getTitleTextFor(this.PAGE_ID_AANVRAGEN_KINDERBIJSLAG + '_AanvragenKinderbijslag_1');
+  }
+
+  get pageTitleDefaultShortcutFlow(): Promise<string> {
+    return this.getTitleTextFor(this.PAGE_ID_DEFAULT_SHORTCUT_START + '_Start_1');
+  }
+  private getTitleTextFor(id: string): Promise<string> {
+    return element(by.id(id)).element(by.tagName('h1')).getText();
+  }
+
+  retrieveSessionId(sessionName:string): Promise<string> {
+   return browser.executeScript(`return window.sessionStorage.getItem("${sessionName}");`);
+  }
+
+  start(route: string) {
+    browser.get(route);
+  }
+
+  reset() {
+    browser.restart();
+  }
+
+}
