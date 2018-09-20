8c617b716e660851ebb9b754d3ce399d724db9d8
m.van.emmerik <m.van.emmerik@everest.nl>
2018-09-20 15:46:21 +0200

    test(table-filter): Added test to check if the correct element is used when filtering #BQ-5290

--- src/app/modules/list/filter/table.filter-value.component.spec.ts
+++ src/app/modules/list/filter/table.filter-value.component.spec.ts
@@ -1,8 +1,98 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BlueriqSession } from '@blueriq/angular';
 import { FilterOption, FilterValue } from '@blueriq/angular/lists';
+import { BlueriqTestingModule } from '@blueriq/angular/testing';
+import { OwlDateTimeModule } from 'ng-pick-datetime';
+import { OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
+import { MomentTransformer } from '../../form-controls/date/moment-transformer';
 import { TableFilterValueComponent } from '../filter/table.filter-value.component';
+import { ListModule } from '../list.module';
 
 describe('TableFilterValueComponent', () => {
-  const tableFilterValueComponent = new TableFilterValueComponent();
+
+  let tableFilterValueComponent = new TableFilterValueComponent();
+  let fixture: ComponentFixture<TableFilterValueComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MomentTransformer, BlueriqSession],
+      imports: [
+        NoopAnimationsModule,
+        BlueriqTestingModule,
+        ListModule,
+        OwlDateTimeModule,
+        OwlMomentDateTimeModule
+      ]
+    });
+    fixture = TestBed.createComponent(TableFilterValueComponent);
+    tableFilterValueComponent = fixture.componentInstance;
+
+    // mocking the session
+    spyOnProperty(BlueriqSession.prototype, 'language', 'get').and.returnValue({
+      languageCode: 'nl-NL',
+      patterns: {
+        date: 'dd-mm-yyyy',
+        datetime:
+          'dd-mm-yyyy hh:mm:ss'
+      }
+    });
+  });
+
+  it('should render a mat-select when filtering on domain', () => {
+    // setup
+    switchSpecifiedElementByType('domain', 'kip,hond');
+
+    // Verify
+    const specifiedElement = fixture.nativeElement.querySelector('#specifiedElement');
+    const matSelect = specifiedElement.querySelector('mat-select');
+    expect(matSelect).toBeTruthy();
+  });
+
+  it('should render a mat-checkbox when filtering on boolean', () => {
+    // setup
+    switchSpecifiedElementByType('boolean', true);
+
+    // Verify
+    const specifiedElement = fixture.nativeElement.querySelector('#specifiedElement');
+    const matCheckbox = specifiedElement.querySelector('mat-checkbox');
+    expect(matCheckbox.getAttribute('ng-reflect-checked')).toBe('true');
+    expect(matCheckbox).toBeTruthy();
+  });
+
+  it('should render a owl-date when filtering on date', () => {
+    // setup
+    switchSpecifiedElementByType('date', '2012-12-12');
+
+    // Verify
+    const specifiedElement = fixture.nativeElement.querySelector('#specifiedElement');
+    expect(specifiedElement.querySelector('owl-date-time')).toBeTruthy();
+    expect(specifiedElement.querySelector('input').value).toBe('12-12-2012',
+      'The format should be dd-mm-yyyy (see mock)');
+  });
+
+  it('should render a owl-date when filtering on datetime', () => {
+    // setup
+    switchSpecifiedElementByType('datetime', '12-12-2012 10:11:12');
+
+    // Verify
+    const specifiedElement = fixture.nativeElement.querySelector('#specifiedElement');
+    expect(specifiedElement.querySelector('owl-date-time')).toBeTruthy();
+    expect(specifiedElement.querySelector('input')
+                           .getAttribute('ng-reflect-value'))
+    .toBe('12-12-2012 10:11:12', 'The format should be dd-mm-yyyy hh:mm:ss (see mock)');
+  });
+
+  it('should render a inputfield by default', () => {
+    // setup
+    switchSpecifiedElementByType('other', 'some value');
+
+    // Verify
+    const specifiedElement = fixture.nativeElement.querySelector('#specifiedElement');
+    const matInput = specifiedElement.querySelector('input');
+    expect(matInput).toBeTruthy();
+    expect(matInput.value).toBe('some value');
+  });
 
   it('FilterValue operations', () => {
     // setup
@@ -55,4 +145,13 @@ describe('TableFilterValueComponent', () => {
     expect(receivedEvent).toBeTruthy();
     expect(receivedEvent).toEqual('remove me');
   });
+
+  function switchSpecifiedElementByType(type, value) {
+    tableFilterValueComponent.filterValue = new FilterValue();
+    tableFilterValueComponent.filterValue.value = value;
+    tableFilterValueComponent.filterValue.selectedOption = new FilterOption();
+    tableFilterValueComponent.filterValue.selectedOption.type = type;
+    fixture.detectChanges();
+  }
 });
+
