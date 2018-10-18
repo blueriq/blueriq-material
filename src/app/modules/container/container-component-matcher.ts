3a58009327548f7db20aba51ec8f7474158ba931
m.van.emmerik <m.van.emmerik@everest.nl>
2018-10-18 11:53:27 +0200

    feat(container):
    - added toString for component matcher
    
    #BQ-5912

--- src/app/modules/container/container-component-matcher.ts
+++ src/app/modules/container/container-component-matcher.ts
@@ -5,6 +5,11 @@ import { BqIconDirective } from '@shared/directive/icon/icon.directive';
 export class ContainerComponentMatcher implements ComponentMatcher<Container> {
   matches(element: Container): boolean {
     // Has displayName or has an icon or has any child elements
-    return (element.displayName ? true : false) || BqIconDirective.hasIcon(element.styles) || element.children.length > 0;
+    return !!element.displayName || BqIconDirective.hasIcon(element.styles) || element.children.length > 0;
+  }
+
+  /* This will be printed in the development console overview */
+  toString() {
+    return '[displayName], BqIconDirective.hasIcon(element.styles), children.length > 0 ';
   }
 }
