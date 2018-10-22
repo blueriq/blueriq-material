a4532092f81ef930f3d7438b1bd3f517cb58632d
m.van.emmerik <m.van.emmerik@everest.nl>
2018-10-22 14:14:25 +0200

    test(tableheadercolumn):
    - added test for sorting on a column
    
    #BQ-5601

--- src/app/modules/list/header/header.component.spec.ts
+++ src/app/modules/list/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { Filter, FilterOption, FilterValue, List, TableColumn } from '@blueriq/angular/lists';
+import { ButtonTemplate } from '@blueriq/core/testing';
 import { of } from 'rxjs';
 import { TableHeaderColumnComponent } from './header.component';
 
@@ -6,7 +7,7 @@ describe('TableHeaderColumnComponent', () => {
 
   let headerComponent: TableHeaderColumnComponent;
 
-  fdescribe('filtering', () => {
+  describe('filtering', () => {
 
     let listSpy: List;
     let filterSpy: Filter;
@@ -58,80 +59,46 @@ describe('TableHeaderColumnComponent', () => {
 
   });
 
-  fdescribe('sorting', () => {
+  describe('sorting', () => {
 
-//   let button: ButtonTemplate;
-//   let session: BlueriqTestSession;
-//   let component: ComponentFixture<TableSortComponent>;
-//
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         NoopAnimationsModule,
-//         BlueriqTestingModule,
-//         ListModule
-//       ]
-//     });
+    let listSpy: List;
+    let filterSpy: Filter;
+    let columnSpy;
 
-    //   beforeEach(() => {
-//     button = ButtonTemplate.create().styles('sort');
-//     session = BlueriqSessionTemplate.create().build(button);
-//     component = session.get(TableSortComponent);
-//     component.autoDetectChanges();
-//   });
-//
-//   it('should display the right material icon', () => {
-//     let iconElement = component.debugElement.query(By.css('.material-icons')).nativeElement;
-//     let currentIcon = component.componentInstance.getIconByDirection();
-//     expect(iconElement).not.toContain('arrow_upward');
-//     expect(iconElement).not.toContain('arrow_downward');
-//     expect(currentIcon).toBe('');
-//
-//     session.update(
-//       button.styles('icon', 'descending')
-//     );
-//     iconElement = component.nativeElement.querySelector('.material-icons').innerHTML;
-//     currentIcon = component.componentInstance.getIconByDirection();
-//     expect(iconElement).toContain('arrow_upward');
-//     expect(currentIcon).toBe('arrow_upward');
-//
-//     session.update(
-//       button.styles('icon', 'ascending')
-//     );
-//     iconElement = component.nativeElement.querySelector('.material-icons').innerHTML;
-//     currentIcon = component.componentInstance.getIconByDirection();
-//     expect(iconElement).toContain('arrow_downward');
-//     expect(currentIcon).toBe('arrow_downward');
-//   });
-//
-//   it('should have a working hovering', () => {
-//     let isHovering = component.componentInstance.hovering;
-//     let currentIcon = component.componentInstance.getIconByDirection();
-//     expect(isHovering).toBeFalsy();
-//     expect(currentIcon).toBe('');
-//
-//     fireEvent(component.nativeElement.querySelector('.material-icons'), 'mouseenter');
-//     isHovering = component.componentInstance.hovering;
-//     currentIcon = component.componentInstance.getIconByDirection();
-//     expect(isHovering).toBeTruthy();
-//     expect(currentIcon).toBe('arrow_downward');
-//
-//     fireEvent(component.nativeElement.querySelector('.material-icons'), 'mouseleave');
-//     isHovering = component.componentInstance.hovering;
-//     currentIcon = component.componentInstance.getIconByDirection();
-//     expect(isHovering).toBeFalsy();
-//     expect(currentIcon).toBe('');
-//   });
+    beforeEach(() => {
+      filterSpy = jasmine.createSpyObj<Filter>(['addFilter']);
+      listSpy = { filter$: of(filterSpy) } as any;
+      headerComponent = new TableHeaderColumnComponent(listSpy);
+      columnSpy = jasmine.createSpyObj<TableColumn>('TableColumn', ['cellFor']);
+      headerComponent.column = columnSpy;
+    });
 
-    function fireEvent(element, event) {
-      if (element.fireEvent) {
-        element.fireEvent('on' + event);
-      } else {
-        const evObj = document.createEvent('Events');
-        evObj.initEvent(event, true, false);
-        element.dispatchEvent(evObj);
-      }
-    }
+    it('should display the right material icon if no direction', () => {
+      let currentIcon = headerComponent.getIconByDirection();
+      expect(currentIcon).toBe('');
+      expect(headerComponent.isSorting()).toBeFalsy();
+    });
+
+    it('should display the right material icon if direction is descending', () => {
+      const column: TableColumn | any = {
+        sort: ButtonTemplate.create('sort').styles('descending').build()
+      };
+      headerComponent.column = column;
+      let currentIcon = headerComponent.getIconByDirection();
+      expect(currentIcon).toBe('arrow_upward');
+      expect(headerComponent.isSorting()).toBeTruthy();
+    });
+
+    it('should display the right material icon if direction is ascending', () => {
+      const column: TableColumn | any = {
+        sort: ButtonTemplate.create('sort').styles('ascending').build()
+      };
+      headerComponent.column = column;
+      let currentIcon = headerComponent.getIconByDirection();
+      expect(currentIcon).toBe('arrow_downward');
+      expect(headerComponent.isSorting()).toBeTruthy();
+    });
+
   });
 
 });
