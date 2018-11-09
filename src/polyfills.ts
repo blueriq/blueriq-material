65d2d7adbcbf24659778de9a90ab530b85ec8ad1
Martijn van TIlburg <m.van.tilburg@everest.nl>
2018-11-09 15:04:49 +0100

    fix: Reintroduce import for Reflect API to prevent Angular initialization error on prod builds #BQ-5856

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -34,6 +34,12 @@ import 'core-js/es6/weak-map';
 import 'core-js/es7/array'; // BQ-5059 - fixes the button click bug
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
+/** IE10 and IE11 requires the following for the Reflect API. */
+// import 'core-js/es6/reflect';
+/** Evergreen browsers require these. **/
+// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+// BQ-5856 - When removed, for prod builds, Angular fails to initialize because of missing Reflect functions
+import 'core-js/es7/reflect';
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
