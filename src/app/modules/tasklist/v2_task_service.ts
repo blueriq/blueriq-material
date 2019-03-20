29d371b36e0dcea3574adac6e9fabc9c3d6288b6
Felix Janssen <f.janssen@everest.nl>
2019-03-20 11:52:47 +0100

    BQ-6840: update getAlltasks to match the changed return type of Backend.get

--- src/app/modules/tasklist/v2_task_service.ts
+++ src/app/modules/tasklist/v2_task_service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Session } from '@blueriq/angular';
 import { Backend } from '@blueriq/angular/backend/common';
 import { Observable } from 'rxjs';
-import { filter, share } from 'rxjs/operators';
+import { filter, map, share } from 'rxjs/operators';
 import { Task, TaskEvent, TaskService } from './task_service';
 
 /** @internal */
@@ -19,7 +19,7 @@ export class V2TaskService implements TaskService {
   }
 
   getAllTasks(session: Session, containerUuid: string): Observable<Task[]> {
-    return this.backend.get<Task[]>(`/api/v2/session/${ session.sessionId }/tasks/${ containerUuid }`);
+    return this.backend.get<Task[]>(`/api/v2/session/${ session.sessionId }/tasks/${ containerUuid }`).pipe(map(response => response.data));
   }
 
   private initPushMessageObserver(): void {
