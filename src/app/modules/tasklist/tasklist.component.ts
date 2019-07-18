d53771610338eb18b29c5036851cf022ccce67c6
Felix Janssen <f.janssen@blueriq.com>
2019-07-18 09:25:39 +0200

    feat(tasklist): Added support for the noResults message

--- src/app/modules/tasklist/tasklist.component.ts
+++ src/app/modules/tasklist/tasklist.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort, PageEvent } from '@angular/material';
+import { AfterContentChecked, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
 import { BlueriqComponent, BlueriqSession } from '@blueriq/angular';
 import { Button, Container, PresentationStyles } from '@blueriq/core';
+import { Subscription } from 'rxjs';
 import { BqContentStyles } from '../BqContentStyles';
 import { BqPresentationStyles } from '../BqPresentationStyles';
 import { Task } from './task_service';
@@ -18,7 +19,7 @@ import { TaskListDataSource } from './tasklist-datasource';
   type: Container,
   selector: BqContentStyles.TASK_LIST,
 })
-export class TaskListComponent implements OnInit {
+export class TaskListComponent implements OnInit, AfterContentChecked, OnDestroy {
 
   displayedColumns: string[];
 
@@ -32,23 +33,58 @@ export class TaskListComponent implements OnInit {
 
   tasksToHighlight: string[];
 
+  private sortChangeSubscription: Subscription;
+  private tasksSubscription: Subscription;
+  private taskEventsSubscription: Subscription;
+  private data: Task[] = [];
+
   constructor(public taskList: TaskList, session: BlueriqSession) {
     this.taskDataSource = new TaskListDataSource(taskList.columnDefinitions, session.localization.dateFormats);
     this.displayedColumns = taskList.columnDefinitions.map(column => column.identifier);
     this.tasksToHighlight = [];
   }
 
-  ngOnInit(): void {
-    this.taskDataSource.sort = this.sort;
-    this.taskDataSource.paginator = this.paginator;
-    this.sort.sortChange.subscribe(() => {
-      this.clearTasksToHighlight();
-    });
+  get noResultsText(): string {
+    if (this.taskList.noResults) {
+      return this.taskList.noResults.plainText;
+    }
+    return '';
+  }
 
-    this.taskList.tasks$.subscribe(tasks => this.updateDataSource(tasks));
-    this.taskList.taskEvents$.subscribe(taskEvent => {
+  ngOnInit(): void {
+    this.tasksSubscription = this.taskList.tasks$.subscribe(tasks => this.updateDataSource(tasks));
+    this.taskEventsSubscription = this.taskList.taskEvents$.subscribe(taskEvent => {
       this.tasksToHighlight.push(taskEvent.taskModel.identifier);
     });
+  }
+
+  ngAfterContentChecked(): void {
+    if (this.taskDataSource.sort == null || this.taskDataSource.sort !== this.sort) {
+      this.taskDataSource.sort = this.sort;
+    }
+    if (this.taskDataSource.paginator == null || this.taskDataSource.paginator !== this.paginator) {
+      this.taskDataSource.paginator = this.paginator;
+    }
+    if (this.sort != null) {
+      if (this.sortChangeSubscription != null) {
+        this.sortChangeSubscription.unsubscribe();
+      }
+      this.sortChangeSubscription = this.sort.sortChange.subscribe(() => {
+        this.clearTasksToHighlight();
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.sortChangeSubscription != null) {
+      this.sortChangeSubscription.unsubscribe();
+    }
+    if (this.tasksSubscription != null) {
+      this.tasksSubscription.unsubscribe();
+    }
+    if (this.taskEventsSubscription != null) {
+      this.taskEventsSubscription.unsubscribe();
+    }
   }
 
   /** extracts the data that should be shown in the cell that is being rendered */
@@ -76,13 +112,23 @@ export class TaskListComponent implements OnInit {
     this.taskList.buttonPressed(button, taskIdentifier);
   }
 
+  toggleData(): void {
+    if (this.taskDataSource.data.length > 0) {
+      this.data = Object.assign([], this.taskDataSource.data);
+      this.taskDataSource.data = [];
+    } else {
+      this.taskDataSource.data = Object.assign([], this.data);
+      this.data = [];
+    }
+  }
+
   /** passes a new filter value to the datasource */
   applyFilter(filterValue: string): void {
     this.clearTasksToHighlight();
     this.taskDataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  pageChanged(event: PageEvent): void {
+  pageChanged(): void {
     this.clearTasksToHighlight();
   }
 
