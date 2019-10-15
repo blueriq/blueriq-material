5e60be5f3d91a55a30846f2f3015f623b628f656
Felix Janssen <f.janssen@blueriq.com>
2019-10-15 14:28:58 +0200

    e2e: Add an e2e test that validates task list behavior on a single tab #BQ-8778

--- e2e/src/tasklist/tasklist.e2e-spec.ts
+++ e2e/src/tasklist/tasklist.e2e-spec.ts
@@ -5,7 +5,7 @@ describe('TaskList App', () => {
 
   let app: TaskListFlow = new TaskListFlow();
 
-  describe('tasklist', () => {
+  describe('task list', () => {
     beforeEach(async() => {
       await app.reset();
       await app.start();
@@ -24,7 +24,7 @@ describe('TaskList App', () => {
       // current page is still the dashboard, intake page opened on new tab
       app.verifyOnDashboardPage();
 
-      // switch to first tasklist tab
+      // switch to first task list tab
       app.switchToTaskListOneTab();
       app.verifyOnIntakePage();
 
@@ -39,17 +39,17 @@ describe('TaskList App', () => {
 
       // now 3 tabs are opened: the dashboard and two tabs with the same case
 
-      // switch to the first tab with a tasklist
+      // switch to the first tab with a task list
       app.switchToTaskListOneTab();
       app.verifyOnTaskListPage();
 
-      // tasklist should contain 2 tasks
+      // task list should contain 2 tasks
       app.verifyNumberOfTasks(2);
 
       // start task 'Toevoegen bewijsstukken'
       app.startTask('Toevoegen bewijsstukken');
 
-      // verify that other tasklist buttons are now disabled
+      // verify that other task list buttons are now disabled
       app.switchToTaskListTwoTab();
       app.verifyOnTaskListPage();
       // when the task is started, an event is sent back to the frontend that the case is locked, which is what
@@ -67,11 +67,52 @@ describe('TaskList App', () => {
       // check that task == Toevoegen bewijsstukken
       app.executeTaskToevoegenBewijsstukken();
 
-      // back to case overview with tasklist again
+      // back to case overview with task list again
       app.verifyOnTaskListPage();
 
-      // verify that other tasklist buttons are enabled again
+      // verify that other task list buttons are enabled again
       app.switchToTaskListTwoTab();
+      app.verifyOnTaskListPage();
+
+      // as described above, make sure that events can be received
+      browser.sleep(500);
+
+      // a new task Toevoegen bewijsstukken is added, all tasks should be enabled again
+      app.verifyNumberOfTasks(2);
+      app.verifyTaskButtonsAreEnabled();
+    });
+
+    it('should display the correct number of tasks after executing a repeatable task', () => {
+      // login with Aanvrager role
+      app.login('Aanvrager', 'Aanvrager');
+
+      // State: logged in
+      app.verifyOnDashboardPage();
+
+      // start intake
+      app.startIntake();
+
+      // current page is still the dashboard, intake page opened on new tab
+      app.verifyOnDashboardPage();
+
+      // switch to first task list tab
+      app.switchToTaskListOneTab();
+      app.verifyOnIntakePage();
+
+      // start case
+      app.startCase();
+      app.verifyOnTaskListPage();
+
+      // task list should contain 2 tasks
+      app.verifyNumberOfTasks(2);
+
+      // start task 'Toevoegen bewijsstukken'
+      app.startTask('Toevoegen bewijsstukken');
+
+      // check that task == Toevoegen bewijsstukken
+      app.executeTaskToevoegenBewijsstukken();
+
+      // back to case overview with task list again
       app.verifyOnTaskListPage();
 
       // as described above, make sure that events can be received
