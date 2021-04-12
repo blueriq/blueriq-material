3f27f10c834bbe987aba022959f315e811da9a6f
j.koehoorn <j.koehoorn@everest.nl>
2021-04-12 12:18:10 +0200

    fix(datepicker): fallback to English when localization data is not available
    BQ-12086

--- src/app/modules/form-controls/date/datetimepicker/datetimepicker.owl.ts
+++ src/app/modules/form-controls/date/datetimepicker/datetimepicker.owl.ts
@@ -1,3 +1,4 @@
+import { getLocaleId } from '@angular/common';
 import { BlueriqSession } from '@blueriq/angular';
 import {
   DateTimeAdapter,
@@ -11,7 +12,16 @@ import { parseBqLocale } from '@shared/date/bq-date-parser';
 import { OwlDateTimeIntlFactory } from '../../../../configuration/date/owl-datetime-intl-factory';
 
 export function localeFactory(session: BlueriqSession): string {
-  return parseBqLocale(session.localization);
+  const locale = parseBqLocale(session.localization);
+  // BQ-12086: missing Angular locale causes an exception when rendering the datepicker.
+  // A fix is pending in https://github.com/danielmoncada/date-time-picker/pull/115. This can, and should be removed
+  // once that has been incorporated into a release of `@danielmoncada/angular-datetime-picker`.
+  try {
+    getLocaleId(locale);
+  } catch {
+    return 'en-US';
+  }
+  return locale;
 }
 
 export function dateTimeFormatFactory(session: BlueriqSession): OwlDateTimeFormats {
