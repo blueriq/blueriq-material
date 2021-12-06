ad43b67848ad245d16ab248badbc5073220e2d7b
Randy Anft <r.anft@everest.com>
2021-12-06 11:48:21 +0100

    fix(menuItems):made menu items accessible via keyboard

--- src/app/modules/menu/menu-item/menu-item.component.ts
+++ src/app/modules/menu/menu-item/menu-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, QueryList, ViewChildren } from '@angular/core';
+import { MatMenuItem } from '@angular/material/menu';
 import { Button, Container, Element } from '@blueriq/core';
 
 @Component({
@@ -9,6 +10,7 @@ import { Button, Container, Element } from '@blueriq/core';
 export class MenuItemComponent {
 
   @Input() child: Element;
+  @ViewChildren('items') inputs: QueryList<ElementRef<MatMenuItem>>;
 
   asButton(element: Element): Button {
     return element as Button;
@@ -22,7 +24,33 @@ export class MenuItemComponent {
     if (element.parent instanceof Container) {
       return element.parent.contentStyle === 'menubar';
     }
+    console.log(false);
     return false;
   }
 
+  public onMenuKeyDown(event: KeyboardEvent, index: number) {
+    let menuItem = this.asContainer(this.child).children;
+    let lastIndex = this.inputs.length - 1;
+    let previousIndex = index - 1;
+    let nextIndex = index + 1;
+
+    switch (event.key) {
+      case 'ArrowUp':
+        index == 0 ? this.focusElement(menuItem, lastIndex) : this.focusElement(menuItem, previousIndex);
+        break;
+      case 'ArrowDown':
+        index == lastIndex ? this.focusElement(menuItem, 0) : this.focusElement(menuItem, nextIndex);
+        break;
+    }
+  }
+
+  public focusElement(elementArray: Element[], index: number) {
+    let eName = elementArray[index].name || '';
+    if (eName.length > 0) {
+      setTimeout(() => {
+        document.getElementsByName(eName)[0].focus();
+      });
+    }
+
+  }
 }
