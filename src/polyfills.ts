efbc6d2e5fe4639819b2a07133ac035024fa3f9b
j.koehoorn <j.koehoorn@everest.nl>
2022-11-11 15:16:36 +0100

    chore: remove polyfills since IE11 is no longer supported
    The update to Angular 14 means that Internet Explorer 11 can no longer be
    supported, and it was the last browser for which the polyfills were needed.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -16,39 +16,10 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/array';
-import 'core-js/es/date';
-import 'core-js/es/function';
-import 'core-js/es/map';
-import 'core-js/es/math';
-import 'core-js/es/number';
-import 'core-js/es/object';
-import 'core-js/es/parse-float';
-import 'core-js/es/parse-int';
-import 'core-js/es/regexp';
-import 'core-js/es/set';
-import 'core-js/es/string';
-import 'core-js/es/symbol';
-import 'core-js/es/weak-map';
-import 'core-js/es/weak-set';
-/**
- * Required to support EventSource in Edge
- * The built version only resides in the example folder, so this is intended
- */
-import 'eventsource/example/eventsource-polyfill'; // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js'; // Included with Angular CLI.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es/reflect';
-
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-// import 'core-js/es/reflect';
-// import 'core-js/modules/es.object.entries';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
