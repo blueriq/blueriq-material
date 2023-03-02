c3011db87d76ee8351efb20d6d15c06faa406b99
Terry van der Griend <t.van.der.griend@everest.nl>
2023-03-02 14:34:39 +0000

    BQ-17097 update table header icon test cases

--- src/app/modules/list/header/header.component.spec.ts
+++ src/app/modules/list/header/header.component.spec.ts
@@ -17,22 +17,22 @@ describe('TableHeaderColumnComponent', () => {
       expect(currentIcon).toBe('');
     });
 
-    it('should choose a downward array even when not ordering, as that will be shown on hover', () => {
+    it('should choose a ascending icon even when not ordering, as that will be shown on hover', () => {
       headerComponent.column = { ordering: 'none' } as TableColumn;
       const currentIcon = headerComponent.getIconByDirection();
-      expect(currentIcon).toBe('arrow_downward');
+      expect(currentIcon).toBe('arrow_upward');
     });
 
     it('should display the right material icon if direction is descending', () => {
       headerComponent.column = { ordering: 'descending' } as TableColumn;
       const currentIcon = headerComponent.getIconByDirection();
-      expect(currentIcon).toBe('arrow_upward');
+      expect(currentIcon).toBe('arrow_downward');
     });
 
     it('should display the right material icon if direction is ascending', () => {
       headerComponent.column = { ordering: 'ascending' } as TableColumn;
       const currentIcon = headerComponent.getIconByDirection();
-      expect(currentIcon).toBe('arrow_downward');
+      expect(currentIcon).toBe('arrow_upward');
     });
 
   });
