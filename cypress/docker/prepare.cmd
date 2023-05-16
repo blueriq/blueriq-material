b0bf310c19f8c131c1a100ababed4b035a501fdd
Roy Bissels <r.bissels@everest.nl>
2023-05-16 13:18:40 +0000

    Resolve BQ-19762 "Fix e2e"

--- cypress/docker/prepare.cmd
+++ cypress/docker/prepare.cmd
@@ -1,15 +1,81 @@
 :: Init
-set $LOCATION="./cypress/docker/preparations"
-set $BQ_VERSION="15.9.2.800"
-set $CDS_VERSION="4.1.1"
+set location="./cypress/docker/preparations"
+set checkoutDir=".\runtime-clone"
+set overrideVersion=false
+set skipBuild=false
+set composeOnly=false
+set runtimeBranch="master"
+set runtimeUrl="git@bq-gitlab.everest.nl:blueriq/blueriq.git"
+set runtimeVersion="15.12.0.1026"
+set cdsVersion="4.2.0"
 
-:: Wars
-call mvn dependency:copy "-Dartifact=com.blueriq:blueriq-runtime-application:%$BQ_VERSION%:war" "-DoutputDirectory=%$LOCATION%" "-Dproject.basedir=%$LOCATION%"
-call mvn dependency:copy "-Dartifact=com.blueriq:blueriq-customerdata-sql-store-standalone:%$CDS_VERSION%:jar" "-DoutputDirectory=%$LOCATION%"
+@echo off
+call :read_params %*
+@echo on
 
-:: Copy
-xcopy /I dist cypress\docker\preparations\dist /Y
+if %composeOnly% == false (
+  :: Clean up preparations
+  if exist %location% call rmdir /s /q %location%
+  :: Wars
+  if %overrideVersion% == true (
+    call :build_runtime || exit /b
+  ) else (
+    call :download_runtime || exit /b
+  )
+  call :download_services || exit /b
+
+  :: Copy
+  call xcopy /I dist cypress\docker\preparations\dist /Y
+)
 
 :: Docker build and start
-docker-compose --compatibility --file ./cypress/docker/docker-compose.yml build
-docker-compose --compatibility --file ./cypress/docker/docker-compose.yml up -d
+call docker-compose --compatibility --file ./cypress/docker/docker-compose.yml up -d --build
+exit %ERROR_LEVEL%
+
+
+:read_params
+if not %1/==/ (
+    if not "%__var%"=="" (
+        if not "%__var:~0,1%"=="-" (
+            endlocal
+            goto read_params
+        )
+        endlocal & set %__var:~1%=%~1
+    ) else (
+        setlocal & set __var=%~1
+    )
+    shift
+    goto read_params
+)
+exit /B
+
+:build_runtime
+if %skipBuild% == false (
+  if exist %checkoutDir% call rmdir /s /q %checkoutDir%
+  call git clone --depth 1 --single-branch --branch %runtimeBranch% %runtimeUrl% %checkoutDir% || exit /b
+)
+cd %checkoutDir%\Runtime
+if %skipBuild% == false (
+  call mvn -B clean package -DskipTests -P!quality -am -pl runtime/blueriq-runtime-standalone || exit /b
+)
+
+call :get_version com.blueriq.customerdata.api.version cdsVersion
+call xcopy /I runtime\blueriq-runtime-standalone\target\*.jar  ..\..\cypress\docker\preparations
+cd ..\..\
+exit /B
+
+:download_runtime
+call mvn -B  dependency:copy "-Dartifact=com.blueriq:blueriq-runtime-standalone:%runtimeVersion%:standalone" "-DoutputDirectory=%location%" "-Dproject.basedir=%location%"
+exit /B
+
+:download_services
+call mvn -B  dependency:copy "-Dartifact=com.blueriq:blueriq-customerdata-sql-store-standalone:%cdsVersion%:jar" "-DoutputDirectory=%location%" || exit /b
+exit /B
+
+:get_version
+set expression=%~1
+set storeVar=%~2
+call mvn -B help:evaluate -Dexpression=%expression% -q -DforceStdout > tempVersion.txt
+call set /p %storeVar%=<tempVersion.txt
+call DEL /F /Q tempVersion.txt
+exit /B
