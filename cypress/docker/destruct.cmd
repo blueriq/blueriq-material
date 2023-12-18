10bc84b73b237890361d319eec87febb2ad134f8
Vincent Jansen <v.jansen@blueriq.com>
2023-12-18 12:10:56 +0000

    BQ-16186 DockerRmiLocal - blueriq-material

--- cypress/docker/destruct.cmd
+++ cypress/docker/destruct.cmd
@@ -1,3 +1,3 @@
 mkdir tmp
 docker-compose --file ./cypress/docker/docker-compose.yml logs --tail=all > ./tmp/e2e-logs.txt
-docker-compose --file ./cypress/docker/docker-compose.yml down --rmi all -v
+docker-compose --file ./cypress/docker/docker-compose.yml down --rmi local -v
