3fa7ead99caabdd07a41fb25769af2a476bee98b
m.van.emmerik <m.van.emmerik@blueriq.com>
2024-07-08 09:41:12 +0200

    chore: update suppression file and dep check script

--- dependency-check.groovy
+++ dependency-check.groovy
@@ -1,54 +1,54 @@
 package jenkins
 
 pipeline {
-	agent {
-		label 'master'
-	}
-	options {
-		buildDiscarder(logRotator(numToKeepStr: '3'))
-		disableConcurrentBuilds()
-	}
-	tools  {
-        jdk 'jdk-21.0.1'
-        nodejs 'nodejs-18.16.0'
-	}
-    environment { 
-        CYPRESS_INSTALL_BINARY = '0'
+  agent {
+    label 'master'
+  }
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '3'))
+    disableConcurrentBuilds()
+  }
+  tools {
+    jdk 'jdk-21.0.1'
+    nodejs 'nodejs-18.16.0'
+  }
+  environment {
+    CYPRESS_INSTALL_BINARY = '0'
+  }
+  stages {
+    stage('Checkout SCM') {
+      steps {
+        script {
+          def scmVars = checkout scm
+          env.GIT_COMMIT = scmVars.GIT_COMMIT
+        }
+      }
     }
-	stages {
-		stage('Checkout SCM') {
-			steps {
-				script {
-					def scmVars = checkout scm
-					env.GIT_COMMIT = scmVars.GIT_COMMIT
-				}
-			}
-		}
-		stage('Install Packages') {
-			steps {
-                bat 'call yarn install'
-			}
-		}
-		stage('Analyze') {
-			steps {
-                bat 'md reports'
-                bat 'npx auditjs@4.0.36 ossi --prod -x > reports/scan_node_modules.xml'
-			}
-			post {
-				always {
-					archiveArtifacts '**/reports/*.xml'
-                    junit testResults: 'reports/scan_node_modules.xml'
-				}
-			}
-		}
-	}
-	post {
-		always {
-			step([$class: 'Mailer',
-				  sendToIndividuals: false,
-				  recipients: emailextrecipients([culprits(), requestor()]) + " ${DEVELOPERS_EMAIL}  m.van.emmerik@everest.nl"
-			])
-			deleteDir()
-		}
-	}
+    stage('Install Packages') {
+      steps {
+        bat 'call yarn install'
+      }
+    }
+    stage('Analyze') {
+      steps {
+        bat 'md reports'
+        bat 'npx auditjs@4.0.36 ossi --prod -x --whitelist auditjs-suppressions.json > reports/scan_node_modules.xml'
+      }
+      post {
+        always {
+          archiveArtifacts '**/reports/*.xml'
+          junit testResults: 'reports/scan_node_modules.xml'
+        }
+      }
+    }
+  }
+  post {
+    always {
+      step([$class           : 'Mailer',
+            sendToIndividuals: false,
+            recipients       : emailextrecipients([culprits(), requestor()]) + " ${DEVELOPERS_EMAIL}  m.van.emmerik@everest.nl"
+      ])
+      deleteDir()
+    }
+  }
 }
