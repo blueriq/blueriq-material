2c455c45b6d2c488c6364ed4d164f06d39a29cf5
j.koehoorn <j.koehoorn@everest.nl>
2026-05-26 12:26:12 +0200

    build: use Yarn audit

--- dependency-check.groovy
+++ dependency-check.groovy
@@ -1,47 +1,5 @@
-#!groovy
+@Library('cve-checks') _
 
-pipeline {
-  agent {
-    label 'master'
-  }
-  options {
-    buildDiscarder(logRotator(numToKeepStr: '3'))
-    disableConcurrentBuilds()
-  }
-  tools {
-    nodejs 'nodejs-22.14.0'
-  }
-  environment {
-    CYPRESS_INSTALL_BINARY = '0'
-    OSSINDEX_USERNAME = 'support@blueriq.com'
-    OSSINDEX_TOKEN = credentials('ossindex-sonatype-api-key')
-  }
-  stages {
-    stage('Install Packages') {
-      steps {
-        bat 'call yarn install'
-      }
-    }
-    stage('Analyze') {
-      steps {
-        bat 'md reports'
-        bat 'yarn --silent auditjs ossi --prod -x --user "%OSSINDEX_USERNAME%" --password "%OSSINDEX_TOKEN%" --whitelist auditjs-suppressions.json > reports/scan_node_modules.xml'
-      }
-      post {
-        always {
-          archiveArtifacts '**/reports/*.xml'
-          junit testResults: 'reports/scan_node_modules.xml'
-        }
-      }
-    }
-  }
-  post {
-    always {
-      step([$class           : 'Mailer',
-            sendToIndividuals: false,
-            recipients       : emailextrecipients([culprits(), requestor()]) + " ${DEVELOPERS_EMAIL} m.van.emmerik@everest.nl"
-      ])
-      deleteDir()
-    }
-  }
+nodePipeline {
+  cveCheck.yarn1(name: 'material-theme')
 }
